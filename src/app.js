11126d6388ebcac6f94ce6634ee13920a081faf6
aledrb <alejandradrb@gmail.com>
2021-05-26 01:40:26 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -8,13 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
+  let what = ["my homework", "the keys", "the car", "my money"];
   let when = [
     "before the class",
     "right on time",
     "when I finished",
-    "during my lunch",
-    "while I was praying"
+    "during my lunch"
   ];
 
   var random = Math.floor(Math.random() * 4);
@@ -28,4 +27,14 @@ window.onload = function() {
       when[random] +
       " "
   );
+  let parrafo = document.getElementById("excuse");
+  parrafo.innerHTML =
+    who[random] +
+    " " +
+    action[random] +
+    " " +
+    what[random] +
+    " " +
+    when[random] +
+    " ";
 };
